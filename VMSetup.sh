83b3e1548519ef62865fcd0779485ec8cd81aa8d
drogonj <drogonjm@gmail.com>
2025-06-19 18:13:13 +0200

    first commit

---
+++ VMSetup.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# Add 8.8.8.8 DNS
+# sudo echo "8.8.8.8" >> /etc/resolv.conf wont work
+echo '8.8.8.8' | sudo tee -a /etc/resolv.conf
+
+# pacman ParallelDownloads = 15
+sudo sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 15/' /etc/pacman.conf
+
+#Pacman.conf enable color output
+sudo sed -i 's/#Color/Color/' /etc/pacman.conf
+
+# pacman update and upgrade
+sudo pacman -Sy --noconfirm archlinux-keyring
+sudo pacman -Syu --noconfirm
+
+sudo pacman -S --needed --noconfirm git base-devel
+
+# yay install
+git clone https://aur.archlinux.org/yay-bin.git
+chmod 777 -R yay-bin
+cd yay-bin && makepkg -si --noconfirm
+
+# Install packages using yay
+# Yay is slow so we use it only for required packages
+# and use pacman for the rest
+yay -S --noconfirm \
+        vpcs \
+        dynamips \
+        ubridge \
+        gns3-server \
+        gns3-gui
+
+sudo pacman -Sy --noconfirm \
+            libvirt \
+            inetutils \
+            glibc \
+            xorg \
+            zellij \
+            docker \
+            docker-compose
+
+# Config zellij multiplexer
+echo 'if [ -z "$ZELLIJ" ]; then zellij; fi' >> /home/vagrant/.bashrc
+
+# Set SSH X11Forwarding
+sudo sed -i 's/#X11Forwarding no/X11Forwarding yes/' /etc/ssh/sshd_config
+sudo systemctl restart sshd
---
+++ Vagrantfile
@@ -0,0 +1,35 @@
+username = `whoami`.strip
+
+nodes = [
+    { :hostname => username + 'VM', :ip => '192.168.56.110'},
+]
+
+Vagrant.configure("2") do |config|
+    nodes.each do |node|
+        config.vm.define node[:hostname] do |nodeconfig|
+            nodeconfig.vm.provider "virtualbox" do |v|
+                v.name = node[:hostname]
+                v.cpus = 4
+                v.memory = 4096
+                v.customize ["modifyvm", :id, "--vram", "128"]
+                v.customize ["modifyvm", :id, "--graphicscontroller", "vmsvga"]
+            end
+            nodeconfig.vm.box = "generic/arch"
+            nodeconfig.vm.hostname = node[:hostname]
+
+            nodeconfig.vm.network "private_network", ip: node[:ip]
+            nodeconfig.ssh.insert_key = false
+
+            nodeconfig.vm.synced_folder ".", "/shared", type: "virtualbox"
+
+            # Execute VM Setup script
+            nodeconfig.vm.provision "shell", path: "./VMSetup.sh", privileged: false
+
+            # Restart VM
+            nodeconfig.vm.provision "shell", inline: <<-SHELL
+                echo "Restarting VM..."
+                sudo shutdown -r now
+            SHELL
+        end 
+    end
+end
